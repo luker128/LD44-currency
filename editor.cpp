1db2c5dfc055d33040c886019c3ec69df9aaf31c
Lukasz Dabrowski <lukash.@gmail.com>
2019-05-28 21:50:32 +0200

    Faces work 90%

--- editor.cpp
+++ editor.cpp
@@ -496,15 +496,18 @@ class Editor {
       for (const auto& p: pointsById) {
         file << p->x << " " << p->y << std::endl;
       }
-      file << polygons.size() << std::endl;
-      for (Polygon* polygon: polygons) {
-        file << polygon->textureId << " ";
-        file << polygon->textureScale << " ";
-        file << polygon->textureX << " ";
-        file << polygon->textureY << " ";
-        file << polygon->points.size();
-        for (Point* point: polygon->points) {
-          file << " " << pointIds[point];
+      file << faces.size() << std::endl;
+      for (Face* face: faces) {
+        file << face->textureId << " ";
+        file << face->textureScale << " ";
+        file << face->textureX << " ";
+        file << face->textureY << " ";
+        file << face->triangles.size();
+        for (Triangle* triangle: face->triangles) {
+          for (Edge* edge: triangle->edges) {
+            Point* point = edge->a;
+            file << " " << pointIds[point];
+          }
         }
         file << std::endl;
       }
@@ -526,32 +529,47 @@ class Editor {
       std::map<std::pair<Point*, Point*>, Edge*> edgeMap;
       for (int i=0; i<numFaces; i++) {
         int textureId;
-        int numPoints;
+        int numTriangles;
         float textureScale;
         float textureX;
         float textureY;
-        file >> textureId >> textureScale >> textureX >> textureY >> numPoints;
+        file >> textureId >> textureScale >> textureX >> textureY >> numTriangles;
+        std::vector<Point*> pointsInPoly;
+        for (int j=0; j<numTriangles*3; j++) {
+          int pointId;
+          file >> pointId;
+          pointsInPoly.push_back(points[pointId]);
+        }
         Face* face = new Face;
         face->textureId = textureId;
         face->textureScale = textureScale;
         face->textureX = textureX;
         face->textureY = textureY;
-        face->triangles.push_back(new Triangle);
-        for (int j=0; j<numPoints-1; j++) {
-          Point* a = pointsInPoly[j];
-          Point* b = pointsInPoly[j+1];
-          auto key = std::make_pair(a, b);
-          Edge* edge;
-          auto it = edgeMap.find(key);
-          if (it == edgeMap.end()) {
-            edge = new Edge(*a, *b);
-            edgeMap.insert(std::make_pair(key, edge));
+        for (int triId=0; triId<numTriangles; triId++) {
+          Triangle* triangle = new Triangle();
+          for (int pointId=0; pointId<3; pointId++) {
+            Point* a = pointsInPoly[triId*3 + pointId];
+            Point* b;
+            if (pointId == 2) {
+              b = pointsInPoly[triId*3]; // wrap back to start
+            }
+            else {
+              b = pointsInPoly[triId*3 + pointId + 1];
+            }
+            auto key = std::make_pair(a, b);
+            Edge* edge;
+            auto it = edgeMap.find(key);
+            if (it == edgeMap.end()) {
+              edge = new Edge(*a, *b);
+              edgeMap.insert(std::make_pair(key, edge));
+            }
+            else {
+              edge = it->second;
+              edge->shared = true;
+            }
+            triangle->edges.push_back(edge);
           }
-          else {
-            edge = it->second;
-            edge->shared = true;
-          }
-          face->triangles[0]->edges.push_back(edge);
+          face->triangles.push_back(triangle);
         }
         faces.push_back(face);
       }
